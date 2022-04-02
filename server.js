b738ebf9cc66e7ced9487f6092130ec7ed075176
t2wole <t2wole@gmail.com>
2022-04-02 14:07:32 +0900

    first

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080, function(){
+    console.log('listening on 8080');
+});
+
+//누군가가 /pet 으로 방문을 하면
+//pet관련된 안내문을 띄워주자
+
+app.get('/pet', function(req, res){
+    res.send('펫용품 쇼핑가능 페이지입니다.');
+});
